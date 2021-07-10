debb36a87194e8bcc0715a880ff99ae09c71b770
Amit <hntlsquad@gmail.com>
2021-07-10 18:25:51 +0530

    ui

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Voulez vous cacher avec moi?</title>
+        <meta
+          httpEquiv="refresh"
+          content="0; url = https://github.com/cascandaliato/cache-it-with-vercel#readme"
+        />
+      </Head>
+    </div>
+  );
+}
